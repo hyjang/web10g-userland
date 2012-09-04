82a05ca4f63eb664d02cedf08c1a7f3f173990e9
jsestabrook <jsestabrook@c4055d8e-5e61-1ca4-870b-869e54e7ee36>
2012-09-04 19:34:39 +0000

    Initial import.

---
+++ lib/tcpe/tcpe-genl.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2012 The Board of Trustees of the University of Illinois,
+ *                    Carnegie Mellon University.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ */
+#ifndef TCPE_GENL_H
+#define TCPE_GENL_H
+
+struct tcpe_error* tcpe_list_conns(struct tcpe_client*, tcpe_connection_func);
+struct tcpe_error* tcpe_read_conn(struct tcpe_data*, int cid, const tcpe_client*);
+
+#endif /* TCPE_GENL_H */
