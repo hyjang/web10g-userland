b1a5a4b25547ed8a7ed5ec63feb4f36460ca7209
jsestabrook <jsestabrook@c4055d8e-5e61-1ca4-870b-869e54e7ee36>
2013-01-05 22:49:02 +0000

    Add sockinfo and Qt file from earlier, transitional userland.

---
+++ util/Qt/mainwindow.hpp
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+
+#include "EstatsView.hpp"
+#include <QMainWindow>
+
+class QTableView;
+class ConnTableModel;
+//class ProxyModel;
+class VarWindow;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent=0);
+
+protected:
+    void closeEvent(QCloseEvent *event);
+
+private slots:
+    void update();
+    void openStatsWindow();
+    void testSelectionChanged();
+
+private:
+    void createWidgets();
+    void createLayout();
+    void createConnections(); // QT slot connections; need to change name
+
+    estats_agent *agent;
+    
+    QTableView *tableView;
+    ConnTableModel *model;
+//    ProxyModel *proxyModel;
+    VarWindow *varwin;
+};
+
+#endif // MAINWINDOW_HPP
