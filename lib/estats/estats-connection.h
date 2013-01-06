86227e84600d502851ba61a705575ea98ada1c0e
jsestabrook <jsestabrook@c4055d8e-5e61-1ca4-870b-869e54e7ee36>
2013-01-06 19:28:54 +0000

    Add contructor, etc. for connection_list.

---
+++ lib/estats/estats-connection.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2012 The Board of Trustees of the University of Illinois,
+ *                    Carnegie Mellon University.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ */
+#ifndef ESTATS_CONNECTION_H
+#define ESTATS_CONNECTION_H
+
+struct estats_error* estats_connection_list_new(struct estats_connection_list**);
+struct estats_error* estats_connection_list_free(struct estats_connection_list**);
+
+#endif /* ESTATS_CONNECTION_H */
